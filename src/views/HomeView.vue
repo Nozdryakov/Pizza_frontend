2d882b2edc9e382a7a568a6ab1759a84c4b198af
Nozdryakov <nozdryakovtema0603@gmail.com>
2024-02-13 02:43:14 +0200

    Добавил свайпер

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,16 @@
 <template>
   <header-component></header-component>
   <intro-main></intro-main>
+  <stock-swiper></stock-swiper>
 </template>
 
 <script>
 import HeaderComponent from '@/components/Header/HeaderComponent.vue';
 import IntroMain from "@/components/Intro/IntroMain.vue";
+import StockSwiper from "@/components/StocksSwiper/StockSwiper.vue";
 
 export default {
-  components: { IntroMain, HeaderComponent}
+  components: { StockSwiper, IntroMain, HeaderComponent}
 };
 </script>
 
