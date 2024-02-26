4fb416f5014de08e5455e9159883b589a16bd7ab
Nozdryakov <nozdryakovtema0603@gmail.com>
2024-02-26 20:08:25 +0200

    tabs 0.2 карточка

--- src/components/MenuTab/MenuTab.vue
+++ src/components/MenuTab/MenuTab.vue
@@ -15,10 +15,15 @@
       <div class="tabs-content">
         <div v-for="(category, index) in visibleTabs" :key="index">
           <h2>{{ category.id }}</h2>
-          <ul>
-            <li v-for="(product, i) in category.list" :key="i" class="product">
-<!--              {{ product.name }}-->
-              <img src="@/assets/images/swiper-card-1.png" class="tab-img">
+          <ul class="product">
+            <li v-for="(product, i) in category.list" :key="i" class="product__item">
+              <img src="@/assets/images/image-default.png" class="tab-img">
+              <div class="title"> {{ product.name }}</div>
+              <div class="subtitle">{{ product.description }}</div>
+              <div class="buy__block">
+                <p class="price">{{ product.price }} ₴</p>
+                <button class="btn-buy">Выбрать</button>
+              </div>
 
             </li>
           </ul>
@@ -38,50 +43,50 @@ const data = ref({
       id: "Пицца",
       show: true,
       list: [
-        { name: 'Product 1' },
-        { name: 'Product 2' },
-        { name: 'Product 3' },
-        { name: 'Product 4' }
+        { name: 'Домашняя', description: 'Куриные кусочки, сладкий перец, моцарелла, красный лук, соус сладкий чили, соус альфре', price: 10 },
+        { name: 'Product 2', description: 'Описание продукта 2', price: 12 },
+        { name: 'Product 3', description: 'Описание продукта 3', price: 8 },
+        { name: 'Product 4', description: 'Описание продукта 4', price: 15 }
       ]
     },
     {
       id: "Закуски",
       show: false,
       list: [
-        { name: 'Закуски 1' },
-        { name: 'Закуски 2' },
-        { name: 'Закуски 3' },
-        { name: 'Закуски 4' }
+        { name: 'Закуски 1', description: 'Описание закуски 1', price: 5 },
+        { name: 'Закуски 2', description: 'Описание закуски 2', price: 7 },
+        { name: 'Закуски 3', description: 'Описание закуски 3', price: 4 },
+        { name: 'Закуски 4', description: 'Описание закуски 4', price: 6 }
       ]
     },
     {
       id: "Десерты",
       show: false,
       list: [
-        { name: 'Закуски 1' },
-        { name: 'Закуски 2' },
-        { name: 'Закуски 3' },
-        { name: 'Закуски 4' }
+        { name: 'Десерт 1', description: 'Описание десерта 1', price: 3 },
+        { name: 'Десерт 2', description: 'Описание десерта 2', price: 4 },
+        { name: 'Десерт 3', description: 'Описание десерта 3', price: 5 },
+        { name: 'Десерт 4', description: 'Описание десерта 4', price: 6 }
       ]
     },
     {
       id: "Напитки",
       show: false,
       list: [
-        { name: 'Закуски 1' },
-        { name: 'Закуски 2' },
-        { name: 'Закуски 3' },
-        { name: 'Закуски 4' }
+        { name: 'Напиток 1', description: 'Описание напитка 1', price: 2 },
+        { name: 'Напиток 2', description: 'Описание напитка 2', price: 3 },
+        { name: 'Напиток 3', description: 'Описание напитка 3', price: 2 },
+        { name: 'Напиток 4', description: 'Описание напитка 4', price: 4 }
       ]
     },
     {
       id: "Комбо",
       show: false,
       list: [
-        { name: 'Закуски 1' },
-        { name: 'Закуски 2' },
-        { name: 'Закуски 3' },
-        { name: 'Закуски 4' }
+        { name: 'Комбо 1', description: 'Описание комбо 1', price: 20 },
+        { name: 'Комбо 2', description: 'Описание комбо 2', price: 25 },
+        { name: 'Комбо 3', description: 'Описание комбо 3', price: 18 },
+        { name: 'Комбо 4', description: 'Описание комбо 4', price: 30 }
       ]
     }
   ]
@@ -94,7 +99,6 @@ const visibleTabs = computed(() => {
 const showCategory = (itemId) => {
   data.value.list.forEach(item => {
     item.show = item.id === itemId;
-    console.log(itemId);
   });
 };
 </script>
